46d14a2d9f3dbff7d6cb4516b4e54fd8f92446c5
Rifkymoll <Rifkymaulana123@gmail.com>
2022-06-05 16:36:16 +0700

    npm

---
+++ node-package-manager/index.js
@@ -0,0 +1,6 @@
+// when require moduls from npm no need to type (./)
+const _ = require('lodash');
+
+const myOddEvenArray = _.partition([1, 2, 3, 4, 5, 6], (n) => n % 2);
+
+console.log(myOddEvenArray);
