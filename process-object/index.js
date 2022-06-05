b815e815d6baef527a6eb26b4f05b5b28b1776bc
Rifkymoll <Rifkymaulana123@gmail.com>
2022-06-05 16:14:24 +0700

    node js process-object

---
+++ process-object/index.js
@@ -0,0 +1,21 @@
+const initialMemoryUsage = process.memoryUsage().heapUsed;
+const yourName = process.argv[2];
+const environment = process.env.NODE_ENV;
+
+for (let i = 0; i < 10000; i++) {
+    
+}
+
+const currentMemoryUsage = process.memoryUsage().heapUsed;
+
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`);
+console.log(`Penggunaan memori dari , ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
+
+/**
+ * 
+ * Process Objedct
+ * run this code with 
+ * NODE_ENV=development node ./process-object/index.js <your_name> in your terminal
+ * 
+ */
